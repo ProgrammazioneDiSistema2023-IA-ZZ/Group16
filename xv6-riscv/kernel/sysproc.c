7262f96a73d9951279a3a9db0d5db47e48d674f7
Carlos Valeriano <carlosv.2064@gmail.com>
2024-01-20 14:26:44 +0100

    added sigalarm and sigreturn syscalls, process fields, timer interrupts usertrap behavior, alarm tests

--- xv6-riscv/kernel/sysproc.c
+++ xv6-riscv/kernel/sysproc.c
@@ -89,3 +89,28 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_sigalarm(void)
+{
+  int ticks;
+  uint64 addr;
+
+  argint(0, &ticks);
+  argaddr(1, &addr);
+
+  myproc()->ticks = ticks;
+  myproc()->handler = addr;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  memmove(p->trapframe, p->alarm_tf, PGSIZE);   // restore the user trapframe from the saved alarm trapframe
+  kfree(p->alarm_tf);                           // free the memory allocated for the saved alarm trap trapframe.
+  p->cur_ticks = 0;
+  p->alarm_on = 0;
+  return 0;
+}
